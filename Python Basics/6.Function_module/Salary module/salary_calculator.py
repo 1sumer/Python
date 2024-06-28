8c866fe7c2334c3a3363ef83812b0353a32c3d15
Sumer Pasha <sameerpasha658@gmail.com>
2024-06-28 11:08:40 +0530

    modified

--- Python Basics/6.Function_module/Salary module/salary_calculator.py
+++ Python Basics/6.Function_module/Salary module/salary_calculator.py
@@ -5,15 +5,12 @@ from incentive import Incentive
 class SalaryCalculator:
     def __init__(self, base_salary):
         self.base_salary_obj = BaseSalary(base_salary)
+        self.deduction_obj = Deduction(base_salary)
+        self.incentive_obj = Incentive(base_salary)
 
     def calculate_net_salary(self, leave_days, target):
         base_salary = self.base_salary_obj.get_base_salary()
-        
-        deduction_obj = Deduction(base_salary)
-        deduction_amount = deduction_obj.calculate_deduction(leave_days)
-        
-        incentive_obj = Incentive(base_salary)
-        incentive_amount = incentive_obj.calculate_incentive(target)
-
+        deduction_amount = self.deduction_obj.calculate_deduction(leave_days)
+        incentive_amount = self.incentive_obj.calculate_incentive(target)
         net_salary = base_salary - deduction_amount + incentive_amount
         return net_salary
