faff6a009cb4fd968e1939866d0d070c30b83900
Sumer Pasha <sameerpasha658@gmail.com>
2025-02-14 17:10:54 +0530

    Modified

--- Python_Basics/6.Function_module/Calculator/Calculator.py
+++ Python_Basics/6.Function_module/Calculator/Calculator.py
@@ -1,15 +1,15 @@
 def add(num1,num2):
     return num1 + num2
 
-def sub(num1,num2):
+def subtract(num1,num2):
     return num1 - num2
 
-def mul(num1,num2):
+def multiply(num1,num2):
     return num1 * num2
 
-def div(num1,num2):
+def divide(num1,num2):
     if num2 != 0:
-        return num1 * num2
+        return num1 / num2
     else:
         return "Error: Division by zero"
     
--- Python_Basics/6.Function_module/Calculator/main.py
+++ Python_Basics/6.Function_module/Calculator/main.py
@@ -5,10 +5,10 @@ print("2. Subtraction")
 print("3. Multiplication")
 print("4. Division")
 
-choice = input("Enter your choice (1/2/3/4): ")
-
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
+
+choice = input("Enter your choice (1/2/3/4): ")
 
 if choice == '1':
     print("Result:", calculator.add(num1, num2))
@@ -20,10 +20,6 @@ elif choice == '3':
     print("Result:", calculator.multiply(num1, num2))
     
 elif choice == '4':
-    print("Result:", calculator.divide(num1, num2))
-    
+    print("Result:", calculator.divide(num1, num2)) 
 else:
-    pass
-  
-
-
+    print("Invalid choice")
